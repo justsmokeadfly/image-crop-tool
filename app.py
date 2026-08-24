d8dea47827e9d4a235d01347cbae26b4f24d3782
justsmokeadfly <45425399+justsmokeadfly@users.noreply.github.com>
2026-08-24 18:12:09 +0300

    chore: activate app v2.3

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-exec(compile(Path(__file__).with_name("app_v3.py").read_text(encoding="utf-8-sig"), "app_v3.py", "exec"))
+exec(compile(Path(__file__).with_name("app_v4.py").read_text(encoding="utf-8-sig"), "app_v4.py", "exec"))
